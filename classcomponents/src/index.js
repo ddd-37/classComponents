c9ff00ff08a6703572975f3cd8dbe79e4549e3d0
Devon Deason <devon.d.deason@gmail.com>
2019-11-22 18:51:09 -0700

    setup error handling

--- classcomponents/src/index.js
+++ classcomponents/src/index.js
@@ -8,17 +8,32 @@ class App extends React.Component {
     super(props); // Because we're overriding the constructor function build into React.  We need to call super to bring in the parent's constructor function
 
     this.state = {
-      lat: null
+      lat: null,
+      errorMessage: ""
     };
+
+    // Because we to find the location data right when this instance is rendered, we should retrieve the data in the constructor
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({
+          lat: position.coords.latitude
+        });
+      },
+      error => {
+        this.setState({
+          errorMessage: error.message
+        });
+      }
+    );
   }
 
   render() {
-    window.navigator.geolocation.getCurrentPosition(
-      position => console.log(position),
-      error => console.log(error)
+    return (
+      <>
+        <div>Latitude: {this.state.lat}</div>
+        <div>Error: {this.state.errorMessage}</div>
+      </>
     );
-
-    return <div>Latitude:</div>;
   }
 }
 
