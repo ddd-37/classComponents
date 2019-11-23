0831e10de16992a8db9cd5c1e79496ad85d5d2fd
Devon Deason <devon.d.deason@gmail.com>
2019-11-22 18:15:40 -0700

    swtich to class based component

--- classcomponents/src/index.js
+++ classcomponents/src/index.js
@@ -2,10 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
-const App = () => (
-  <div>
-    <SeasonDisplay />
-  </div>
-);
+class App extends React.Component {
+  render() {
+    window.navigator.geolocation.getCurrentPosition(
+      position => console.log(position),
+      error => console.log(error)
+    );
+
+    return <div>Latitude:</div>;
+  }
+}
 
 ReactDOM.render(<App />, document.getElementById("root"));
