e61c6ec1e0d345f2e71abd7efe22412c12698222
Devon Deason <devon.d.deason@gmail.com>
2019-11-22 18:29:42 -0700

    setup state

--- classcomponents/src/index.js
+++ classcomponents/src/index.js
@@ -3,6 +3,15 @@ import ReactDOM from "react-dom";
 import SeasonDisplay from "./SeasonDisplay";
 
 class App extends React.Component {
+  // Constructor is called for every new instance of a class
+  constructor(props) {
+    super(props); // Because we're overriding the constructor function build into React.  We need to call super to bring in the parent's constructor function
+
+    this.state = {
+      lat: null
+    };
+  }
+
   render() {
     window.navigator.geolocation.getCurrentPosition(
       position => console.log(position),
