7794e101bb8e699d3650271d0804b0eaf3a2621c
Devon Deason <devon.d.deason@gmail.com>
2019-11-23 08:50:12 -0700

    add some styles

--- classcomponents/src/SeasonDisplay.js
+++ classcomponents/src/SeasonDisplay.js
@@ -1,4 +1,16 @@
 import React from "react";
+import "./SeasonDisplay.css"; // Beind the scenes webpack will takes the contents out and place it into the index file
+
+const seasonConfig = {
+  summer: {
+    text: "Lets hit the beach",
+    icon: "icon sun"
+  },
+  winter: {
+    text: "Burr, it is chilly",
+    icon: "icon snowflake"
+  }
+};
 
 const getSeason = (lat, month) => {
   if (month > 2 && month < 9) {
@@ -10,7 +22,15 @@ const getSeason = (lat, month) => {
 
 const SeasonDisplay = props => {
   const season = getSeason(props.lat, new Date().getMonth());
-  return <div>Season Display {season}</div>;
+  const { text, icon } = seasonConfig[season];
+
+  return (
+    <div className={`season-display ${season}`}>
+      <i className={`icon-left massive ${icon}`} />
+      <h1>{text}</h1>
+      <i className={`icon-right massive ${icon}`} />
+    </div>
+  );
 };
 
 export default SeasonDisplay;
