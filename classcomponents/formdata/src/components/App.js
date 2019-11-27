ebb333dedd753b543c73faaa63b0c11df8ff4c34
Devon Deason <devon.d.deason@gmail.com>
2019-11-26 17:47:06 -0700

    inital commit

--- classcomponents/formdata/src/components/App.js
+++ classcomponents/formdata/src/components/App.js
@@ -1,14 +1,27 @@
 import React from "react";
+import unsplash from "../api/unsplash";
 import SearchBar from "./SearchBar";
+import { log } from "util";
+import ImageList from "./ImageList";
 
 class App extends React.Component {
-  onFormSubmit = term => {
-    console.log(`onFormSubmit: ${term}`);
+  state = {
+    images: []
   };
+
+  onFormSubmit = async term => {
+    const response = await unsplash.get("/search/photos", {
+      params: { query: term }
+    });
+
+    this.setState({ images: response.data.results });
+  };
+
   render() {
     return (
       <div className="ui container" style={{ marginTop: "10px" }}>
         <SearchBar onSubmit={this.onFormSubmit} />
+        <ImageList images={this.state.images} />
       </div>
     );
   }
